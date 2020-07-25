d5000425285d92679a37bb5ec9ffa60c0934b940
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2020-07-25 13:05:53 -0400

    Plugin integration

--- captcha/captcha.php
+++ captcha/captcha.php
@@ -11,6 +11,42 @@ define( 'CAPTCHA_HEIGHT',	35 );
 // Captcha font file name (in the same folder)
 define( 'CAPTCHA_FONT',		'VeraMono.ttf' );
 
+// Captcha string length
+define( 'CAPTCHA_LENGTH',	5 );
+
+// Captcha image mime type (currently, jpg, png, or bmp)
+define( 'CAPTCHA_MIME',		'image/png' );
+
+// Captcha image file name (extension should match mime)
+define( 'CAPTCHA_NAME',		'captcha.png' );
+
+// Default hashing algorithm
+define( 'CAPTCHA_HASH',		'tiger160,4' );
+
+
+// Captcha field render template
+$templates['tpl_captcha']	= <<<HTML
+	<input type="hidden" name="capA">
+	<input type="text" name="captcha" 
+		placeholder="{lang:forms:captcha:placeholder}" 
+		class="{input_classes}" required> 
+	<img src="{captcha}" alt="{lang:forms:captcha:alt}" 
+		class="{captcha_classes}">
+HTML;
+
+
+define( 'CAPTCHA_LANG',		<<<JSON
+{
+	"forms"		: {
+		"captcha"	: {
+			"placeholder"	: "Captcha",
+			"alt"		: "captcha"
+		}
+	}
+}
+JSON
+);
+
 
 /**
  *  Generate captcha image
@@ -32,6 +68,10 @@ function captcha( string $txt ) {
 	
 	// Height of image
  	$sizey	= config( 'captcha_height', \CAPTCHA_HEIGHT, 'int' );
+	
+	// Image meta info
+	$mime	= config( 'captcha_mime', \CAPTCHA_MIME );
+	$name	= config( 'captcha_name', \CAPTCHA_NAME );
 	
 	// Character length
  	$cl	= strsize( $txt );
@@ -101,10 +141,24 @@ function captcha( string $txt ) {
 	}
 	
 	// Prepare headers
-	sendGenFilePrep( 'image/png', 'captcha.png', 200, false );
+	sendGenFilePrep( $mime, $name, 200, false );
 	
 	// Send generated image and end execution
-	\imagepng( $img );
+	switch( $mime ) {
+		case 'image/png':
+			\imagepng( $img );
+			break;
+			
+		case 'image/jpg':
+		case 'image/jpeg':
+			\imagejpeg( $img );
+			break;
+			
+		case 'image/bmp':
+			\imagebmp( $img );
+			break;
+	}
+	
 	\imagedestroy( $img );
 	shutdown();
 }
@@ -113,14 +167,17 @@ function captcha( string $txt ) {
  *  Generate a captcha set
  */
 function genCaptcha() : array {
+	$clen	= config( 'captcha_length', \CAPTCHA_LENGTH, 'int' );
+	$sh	= config( 'captcha_hash', \CAPTCHA_HASH );
+	
 	// Text sent as an image to user
-	$usr	= genCodeKey( 4 );
+	$usr	= genCodeKey( $clen );
 	
 	// Random nonce
 	$capA	= genAlphaNum();
 	
 	// Combined hash sent as URL and stored in session
-	$capB	= \hash_hmac( 'tiger160,4', $capA, $usr );
+	$capB	= \hash_hmac( $sh, $capA, $usr );
 	
 	sessionCheck();
 	$_SESSION[$capB] = $usr;
@@ -133,8 +190,9 @@ function genCaptcha() : array {
  *  Verify captcha and nonce pair
  */
 function verifyCaptcha( $capA, $capB, $usr ) {
+	$sh	= config( 'captcha_hash', \CAPTCHA_HASH );
 	return 
-	\hash_equals( $capB, \hash_hmac( 'tiger160,4', $capA, $usr ) );
+	\hash_equals( $capB, \hash_hmac( $sh, $capA, $usr ) );
 }
 
 /**
@@ -162,9 +220,112 @@ function addCaptchaRoutes( string $event, array $hook, array $params ) {
 	] );
 }
 
+/**
+ *  Form field classes
+ */
+function addCaptchaClasses( string $event, array $hook, array $params ) {
+	return 
+	\array_merge_recursive( 
+		$hook, [ 'classes' => [ 'captcha_classes' => '' ] ] 
+	);
+}
+
+/**
+ *  Append language placeholders
+ */
+function captchaLang( string $event, array $hook, array $params ) {
+	return 
+	\array_merge_recursive( 
+		$hook, [ 'terms' => decode( \CAPTCHA_LANG ) ] 
+	);
+}
+
+/**
+ *  Configuration check
+ */
+function checkCaptchaConfig( string $event, array $hook, array $params ) {
+	$filter	= [
+		'captcha_font'	=> [
+			'filter'=> \FILTER_VALIDATE_URL,
+			'options' => [
+				'default' => \CAPTCHA_FONT
+			],
+		],
+		'captcha_name'	=> [
+			'filter'	=> \FILTER_SANITIZE_STRING,
+			'options' => [
+				'default' => \CAPTCHA_NAME
+			]
+		],
+		'captcha_mime'	=> [
+			'filter'	=> \FILTER_SANITIZE_STRING,
+			'options' => [
+				'default' => \CAPTCHA_MIME
+			]
+		],
+		'captcha_height'=> [
+			'filter'	=> \FILTER_VALIDATE_INT,
+			'options'	=> [
+				'min_range'	=> 10,
+				'max_range'	=> 50,
+				'default'	=> \CAPTCHA_HEIGHT
+			]
+		],
+		'captcha_length'=> [
+			'filter'	=> \FILTER_VALIDATE_INT,
+			'options'	=> [
+				'min_range'	=> 3,
+				'max_range'	=> 24,
+				'default'	=> \CAPTCHA_LENGTH
+			]
+		],
+		'captcha_hash'	=> [
+			'filter'	=> 
+				\FILTER_SANITIZE_SPECIAL_CHARS,
+			'flags'	=> 
+				\FILTER_FLAG_STRIP_LOW	| 
+				\FILTER_FLAG_STRIP_HIGH	| 
+				\FILTER_FLAG_STRIP_BACKTICK 
+		]
+	];
+	
+	$mime	= [
+		'png'	=> 'image/png',
+		'jpg'	=> 'image/jpg',
+		'jpeg'	=> 'image/jpg',
+		'bmp'	=> 'image/bmp'
+	];
+	$data	= \filter_var_array( $params, $filter, false );
+	
+	// Check file name matches mime
+	$ext	= 
+	\pathinfo( $data['captcha_name'], \PATHINFO_EXTENSION ) ?? '';
+	
+	if ( 
+		empty( $ext ) || 
+		!\in_array( $data['captcha_mime'], $mime, true ) 
+	) {
+		$data['captcha_mime'] = \CAPTCHA_MIME;
+		$data['captcha_name'] = \CAPTCHA_NAME;
+		
+	} elseif ( $data['captcha_mime'] != $mime[$ext] ) {
+		$data['captcha_mime'] = \CAPTCHA_MIME;
+		$data['captcha_name'] = \CAPTCHA_NAME;
+	}
+	
+	if ( isset( $data['captcha_hash'] ) ) {
+		$data['captcha_hash']	= 
+		hashAlgo( ( string ) $data['captcha_hash'], \CAPTCHA_HASH );
+	}
+	
+	return \array_merge( $hook, $data );
+}
 
 // Register events
+hook( [ 'checkconfig',	'checkCaptchaConfig' ] );
 hook( [ 'initroutes',	'addCaptchaRoutes' ] );
+hook( [ 'loadlanguage',	'captchaLang' ] );
 hook( [ 'showcaptcha',	'showCaptcha' ] );
+hook( [ 'loadcssclasses', 'addCaptchaClasses' ] );
 
 
